9e83889762213496881ec353b6e4a96610121686
Nicolas Tito <mrnicolastito@icloud.com>
2022-01-11 14:45:48 +0000

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
-import NavBar from './components/NavBar/index';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import NavBar from './components/NavBar';
+import Form from './pages/Form';
+import Listing from './pages/Listing';
 
 function App() {
   return (
-    <NavBar/>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Listing/>} />
+        <Route path='/form'>
+          <Route path=':movieId' element={<Form/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
